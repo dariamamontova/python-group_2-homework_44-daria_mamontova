5d8f442d0d1bbfaa9a67fb63f2d0368e3ba66192
Daria <mamontova.dasha@gmail.com>
2018-12-24 01:24:31 +0600

    Added OrderForm, OrderFoodForm.

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,8 +1,18 @@
 from django import forms
-from webapp.models import Food
+from webapp.models import Food, Order, OrderFood
 
 
 class FoodForm(forms.ModelForm):
     class Meta:
         model = Food
         exclude = []
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        exclude = []
+
+class OrderFoodForm(forms.ModelForm):
+    class Meta:
+        model = OrderFood
+        exclude = ['order']
