2ea74cd9d56ca6c58c5e38274ab70829ba0eb1f8
Daria <mamontova.dasha@gmail.com>
2018-12-23 20:04:28 +0600

    Added models.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT, related_name='employee', verbose_name='Пользователь')
+    phone = models.CharField(max_length=50, verbose_name='Телефон')
+
+    def __str__(self):
+        return self.user.get_full_name()
+
+
+class Food(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Название')
+    description = models.TextField(max_length=1000, null=True, blank=True, verbose_name='Описание')
+    photo = models.ImageField(verbose_name='Фотография')
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Цена')
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    STATUS_NEW = 'new'
+    STATUS_PREPARING = 'preparing'
+    STATUS_ON_WAY = 'on_way'
+    STATUS_DELIVERED = 'delivered'
+    STATUS_CANCELED = 'canceled'
+
+    STATUS_CHOICES = (
+        (STATUS_NEW, 'Новый'),
+        (STATUS_PREPARING, 'Готовится'),
+        (STATUS_ON_WAY, 'В пути'),
+        (STATUS_DELIVERED, 'Доставлен'),
+        (STATUS_CANCELED, 'Отменён')
+    )
+
+    contact_phone = models.CharField(max_length=50, verbose_name='Контактный телефон')
+    contact_name = models.CharField(max_length=100, verbose_name='Имя клиента')
+    delivery_address = models.CharField(max_length=200, null=True, blank=True, verbose_name='Адрес доставки')
+    status = models.CharField(max_length=20, default=STATUS_NEW, verbose_name='Статус', choices=STATUS_CHOICES)
+    operator = models.ForeignKey(User, null=True, blank=True, related_name='orders', verbose_name='Оператор', on_delete=models.PROTECT)
+    courier = models.ForeignKey(User, null=True, blank=True, related_name='delivered', verbose_name='Курьер', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.contact_phone
+
+
+class OrderFood(models.Model):
+    order = models.ForeignKey(Order, related_name='foods', verbose_name='Заказ', on_delete=models.PROTECT)
+    food = models.ForeignKey(Food, related_name='+', verbose_name='Блюдо', on_delete=models.PROTECT)
+    amount = models.IntegerField(verbose_name='Количество')
+
+    def __str__(self):
+        return str(self.food)
