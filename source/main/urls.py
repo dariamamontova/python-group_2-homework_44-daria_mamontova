1526b81f071570adfda497fd7ecce505c25db9c3
Daria <mamontova.dasha@gmail.com>
2018-12-24 15:44:35 +0600

    Added Food delete, update, Order create, detail, update functions

--- source/main/urls.py
+++ source/main/urls.py
@@ -17,12 +17,24 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from webapp.views import FoodDetailView, FoodCreateView
+from webapp.views import FoodDetailView, FoodCreateView, FoodUpdateView, OrderDetailView, OrderCreateView, \
+    OrderUpdateView, OrderFoodCreateView, OrderListView, UserListView, OrderRejectView, OrderDeliverView, \
+    FoodDeleteView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', OrderListView.as_view(), name='order_list'),
     path('food/<int:pk>', FoodDetailView.as_view(), name='food_detail'),
     path('food/create', FoodCreateView.as_view(), name='food_create'),
+    path('food/update/<int:pk>', FoodUpdateView.as_view(), name='food_update'),
+    path('food/delete/<int:pk>', FoodDeleteView.as_view(), name='food_delete'),
+    path('order/<int:pk>', OrderDetailView.as_view(), name='order_detail'),
+    path('order/create', OrderCreateView.as_view(), name='order_create'),
+    path('order/<int:pk>/update', OrderUpdateView.as_view(), name='order_update'),
+    path('order/<int:pk>/food/create', OrderFoodCreateView.as_view(), name='order_food_create'),
+    path('users', UserListView.as_view(), name='user_list'),
+    path('order/<int:pk>/cancel', OrderRejectView.as_view(), name='reject_order'),
+    path('order/<int:pk>/deliver', OrderDeliverView.as_view(), name='order_deliver'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,8 +1,11 @@
-from django.views.generic import DetailView, CreateView, UpdateView, View, DeleteView
-from django.urls import reverse
+from django.views.generic import DetailView, CreateView, UpdateView, View, ListView, DeleteView
+from django.urls import reverse, reverse_lazy
 from django.http import HttpResponseRedirect
-from webapp.models import Food, Order, OrderFood
-from webapp.forms import FoodForm
+from webapp.models import Food, Order, OrderFood, User
+from webapp.forms import FoodForm, OrderForm, OrderFoodForm
+from django.shortcuts import redirect
+
+
 
 class FoodDetailView(DetailView):
     model = Food
@@ -15,3 +18,82 @@ class FoodCreateView(CreateView):
 
     def get_success_url(self):
         return reverse('food_detail', kwargs={'pk': self.object.pk})
+
+class FoodUpdateView(UpdateView):
+    model = Food
+    template_name = 'food_update.html'
+    form_class = FoodForm
+
+    def get_success_url(self):
+        return reverse('food_detail', kwargs={'pk': self.object.pk})
+
+class FoodDeleteView(DeleteView):
+    model = Food
+    template_name = 'food_delete.html'
+
+    def get_success_url(self):
+        return reverse('order_list')
+
+class OrderCreateView(CreateView):
+    model = Order
+    template_name = 'order_create.html'
+    form_class = OrderForm
+
+    def get_success_url(self):
+        return reverse('order_detail', kwargs={'pk': self.object.pk})
+
+class OrderDetailView(DetailView):
+    model = Order
+    template_name = 'order_detail.html'
+
+
+class OrderUpdateView(UpdateView):
+    model = Order
+    template_name = 'order_update.html'
+    form_class = OrderForm
+
+    def get_success_url(self):
+        return reverse('order_detail', kwargs={'pk': self.object.pk})
+
+class OrderFoodCreateView(CreateView):
+    model = OrderFood
+    form_class = OrderFoodForm
+    template_name = 'order_food_create.html'
+
+    def get_success_url(self):
+        return reverse('order_detail', kwargs={'pk': self.object.order.pk})
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['order'] = Order.objects.get(pk=self.kwargs.get('pk'))
+        return context
+
+    def form_valid(self, form):
+        form.instance.order = Order.objects.get(pk=self.kwargs.get('pk'))
+        return super().form_valid(form)
+
+
+class OrderDeliverView(View):
+    def get(self, *args, **kwargs):
+        self.object = Order.objects.get(pk=self.kwargs.get('pk'))
+        if self.object.status == 'preparing':
+            self.object.status = 'on_way'
+        else:
+            self.object.status = 'delivered'
+        self.object.save()
+        return redirect('order_list')
+#
+class OrderRejectView(View):
+    def get(self, *args, **kwargs):
+        self.object = Order.objects.get(pk=self.kwargs.get('pk'))
+        self.object.status = 'canceled'
+        self.object.save()
+        return redirect('order_list')
+
+class OrderListView(ListView):
+    model = Order
+    template_name = 'order_list.html'
+
+class UserListView(ListView):
+    model = User
+    template_name = 'user_list.html'
